814a8bba1677df1af44dfd36a4b3635629e34c23
Markus Appel <markospoparkos@gmail.com>
2019-02-26 19:43:20 +0100

    Modify eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,7 +9,10 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': 'off',
+    'no-multiple-empty-lines': 'off',
+    'eol-last': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
