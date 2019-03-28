70b9bbddfaf1d09e222227204a46e836ed78f160
Markus Appel <markospoparkos@gmail.com>
2019-03-28 23:51:12 +0100

    Add JsonProperty annotations

--- api/Structs/WeatherResponse.cs
+++ api/Structs/WeatherResponse.cs
@@ -1,16 +1,23 @@
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System;
 
 namespace Structs {
   public struct ResponseItem {
+    [JsonProperty(PropertyName = "day")]
     public DayOfWeek day { get; set; }
+    [JsonProperty(PropertyName = "temp")]
     public double temp { get; set; }
+    [JsonProperty(PropertyName = "humidity")]
     public int humidity { get; set; }
+    [JsonProperty(PropertyName = "windspeed")]
     public double windspeed { get; set; }
   }
 
   public struct WeatherResponse {
+    [JsonProperty(PropertyName = "current")]
     public ResponseItem current { get; }
+    [JsonProperty(PropertyName = "forecast")]
     public IEnumerable<ResponseItem> forecast { get; }
 
     public WeatherResponse(
